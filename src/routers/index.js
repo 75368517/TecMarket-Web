8285328fe332ba842d9c0e76febc4014b4397365
75368517 <75368517@certus.edu.pe>
2021-12-17 18:53:48 -0500

    tercera modificación

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get('/',(req, res) => {
-    res.render('index.html', { title: 'TecMarket - Home' });
+    res.render('index.html', { title: 'TecMarket - Home útima versión' });
 });
 
 router.get('/nosotros',(req, res) => {
